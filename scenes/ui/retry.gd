92494b4b80ce48d5893a0ce797e3f96763bc5398
Davin Kusuma <157018486+vexushi@users.noreply.github.com>
2026-02-07 22:51:35 +0700

    feat: stage 4

---
+++ scenes/ui/retry.gd
@@ -0,0 +1,18 @@
+extends Button
+
+@export_group("Turn Back Time Mechanic")
+@export var turn_back_time: bool = false ## Enable time machine mechanic
+
+var player_original_pos
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	get_tree().reload_current_scene()
